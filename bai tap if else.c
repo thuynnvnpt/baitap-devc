5340670f9696cfe40c4bc018e2ad0b80c3691b6f
thuynnvnpt <cafe.7love88@gmail.com>
2022-06-01 19:02:45 +0700

    aa

---
+++ bai tap if else.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	int num1;
+	int num2;
+	
+	printf(" Enter two inergers, anhd i will tell you\n ");
+	printf("the relationships they satisfy :");
+	
+	scanf("%d%d",&num1,&num2);
+	
+	if (num1==num2){
+	printf("%d is equal to %d\n",num1,num2);
+		}
+		
+		if(num1!=num2)  {
+		printf("%d is not equal to %d\n",num1,num2);
+		}
+if(num1<num2){
+	prinf("%d is less than %d\n",num1,num2);
+}
+}
