b8d420dc83520d09803742ffd0966e77051f0e8f
thuynnvnpt <cafe.7love88@gmail.com>
2022-06-04 21:42:04 +0700

    vidu_6.1

---
+++ vidu_6.1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main()
+ {
+	int a = 10;
+	float b = 24.67892345;
+	char ch = 'A';
+	
+	printf("\nInteger data = %d", a);
+	printf("\nFloat data = %f", b);
+	printf("\nCharacter = %c", ch);
+	printf("\nThis prints the string");
+	printf("%s","\nThis also prints string");
+}
